2b8ee35f89ffa67cedeee401b710c9d949adf906
MatheusH2021 <matheus.henrique.mhms@gmail.com>
2025-09-14 15:12:17 -0300

    Feature: Adição da rota POST

--- controllers/livroController.js
+++ controllers/livroController.js
@@ -1,6 +1,21 @@
 const {Livro: LivroModel, Livro} = require("../models/Livro");
 
 const livroController = {
+    create: async(req, res)=> {
+        const livro = req.body;
+        if (livro.title == "") {
+            return res.status(400).json({ error: "informe o campo obrigatorio: Title." });
+        } else if (livro.author == "") {
+            return res.status(400).json({ error: "informe o campo obrigatorio: Author." });
+        } else if (await LivroModel.findOne( { isbn: livro.isbn })) {
+            return res.status(400).json({ error: "ISBN já cadastrado." });
+        }
+
+        const response = await LivroModel.create(livro);
+        res.status(201).json({response, msg:"Livro cadastrado!"});
+
+    },
+
     readAll: async(req, res)=> {
         let results = await LivroModel.find({});
         res.send(results).status(200);
--- routes/livro.js
+++ routes/livro.js
@@ -2,5 +2,6 @@ const router = require("express").Router();
 const livroController = require("../controllers/livroController");
 
 router.route("/livros/all").get((req, res)=> livroController.readAll(req, res));
+router.route("/livros").post((req, res)=> livroController.create(req, res));
 
 module.exports = router;
